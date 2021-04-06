d6f2c41880a2dce5a2bbb2337d19ba604586196c
siaulee <siaulee@ucsc.edu>
2021-04-06 11:27:21 -0700

    included the left and right functions given to us by the professor and will now begin coding

--- asgn1/lrc.c
+++ asgn1/lrc.c
@@ -3,3 +3,28 @@
 // Author: Simon Lee		    //
 // Class & Term: CSE 13s Spring 2021//
 //////////////////////////////////////
+#include <stdio.h>
+#include "philos.h"
+
+int main(){
+
+}
+//SOURCE: Darell Long Assignment 1 document
+//PARAMETERS:
+//pos: The position of the current player
+//players:  number of players in the game
+static inline uint8_t left_person(uint8_t pos, uint8_t players) {
+       return ((pos+players - 1) % players);	
+}
+//SOURCE: Darell Long Assignment 1 document
+//PARAMETERS:
+//pos: The position of the current player
+//players:  number of players in the game
+
+static inline uint8_t right_person(uint8_t pos, uint8_t players) {
+	return ((pos + 1) % players); 
+}
+
+void roll_dice(){
+
+}
