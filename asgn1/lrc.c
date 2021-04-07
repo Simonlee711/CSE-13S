2a97c5f636b262b34e4cdfb2358331e33f4a4d1e
siaulee <siaulee@ucsc.edu>
2021-04-06 23:28:47 -0700

    made pseudocode to help guide logic and will replace it with c code over time

--- asgn1/lrc.c
+++ asgn1/lrc.c
@@ -12,20 +12,24 @@ int pos = 0;					//position equals 0
 int players = 1;				//minimum players = 1
 int money[14] ;					//money can be assigned up to 14 players - SOURCE: Small Group Tutoring
 int center_pot = 0;				//center pot has no money to begin
-int winner = 0;					//winner is not declared
+int winner = 0;					//winner is not declared - SOURCE: Small Group Tutoring
+
+//dice enumeration used in roll function
+typedef enum faciem { PASS, LEFT, RIGHT, CENTER } faces;
+faces die[] = {LEFT, RIGHT, CENTER, PASS, PASS, PASS };
 
 int main(){
   
   //ask for seed to be randomized
   int seed;
-  printf("random seed here: ");
-  scanf("%d", &seed);
-  srand(seed);
+  printf("Randome Seed: ");
+  scanf("%u", &seed);
+  srandom(seed);
   
   //ask for players
-  printf("How many players? ")
-  scanf("%d", players)
-  if (players < 2 || players > 14){		//if not in range 2<i<14. end program		
+  printf("Number of players: ")
+  scanf("%u", players)
+  if (players < 1 || players > 14){		//if not in range 2<i<14. end program		
     return 0
   }
   else(){
@@ -36,15 +40,14 @@ int main(){
     }	
  
   //actual game portion
-  while(winner = 1){				//while loop to simulate game
-    int temp = money[pos];
-    if(money >= 3){				//if money is >= 3 roll 3 times
-      for(i = 0; i < 3; i++){			//for loop iterates 3 times
-	roll();					//function roll is called 3 times
+  while(winner != 1){				//while loop to simulate game
+    int track_money = money[pos];
+    if(track_money >= 3){				//if money is >= 3 roll 3 times
+	money[i] = roll(3);					//function roll is called 3 times
       }
       //move index to the right once
-      if(/*if only one player has atleast 1$*/){				//if one player remains declare winner
-	  game_winner = players[i]; 		//winner is player[i]
+      if( ){				//if one player remains declare winner
+	  game_winner = players[pos]; 		//winner is player[i]
 	  printf("winner is", game_winner); 	//print winner
           winner += 1;				//prompt condition to exit while loop
       }
@@ -76,8 +79,20 @@ static inline uint8_t right_person(uint8_t pos, uint8_t players) {
   return ((pos + 1) % players); 
 }
 
-void roll_dice(int pos, int players){
-  int random = 0;
+void roll_dice(int num_rolls){
+  for (i = 0; i < num_rolls; i++){
+   roll = die[random() % 6];
+   if(roll == 0){ //pass left because enum index 0 is Left
+   }
+   if(roll == 1){ //pass right because enum index 1 is right
+   }
+   if(roll == 2){ //put in center because enum index 2 is center
+   }
+   else(){ //return back to function
+   }
+  return //money
+  }
+  
 
 }
 
