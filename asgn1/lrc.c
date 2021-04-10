b647ef81f3a2b69a6a2b2068238c39a36c896713
siaulee <siaulee@ucsc.edu>
2021-04-10 13:17:18 -0700

    formatted to pass pipeline test

--- asgn1/lrc.c
+++ asgn1/lrc.c
@@ -47,7 +47,7 @@ int main(void) {
     uint32_t valid_players = scanf("%u", &players);
     if (valid_players != 1 || players > 14 || players < 2) {
         fprintf(stderr, "Invalid number of players\n"); //tutor Tristan taught me to use fprintf
-	return 1;
+        return 1;
     }
 
     //assign money
@@ -64,41 +64,41 @@ int main(void) {
     while (players_in > 1) {
         if (money[pos]
             >= 3) { //Eugene's section broke down the structure of how to determine the rolls
-            dice_roll = 3;	
-        }else{
-	    dice_roll = money[pos];
-	}
+            dice_roll = 3;
+        } else {
+            dice_roll = money[pos];
+        }
 
         //dice enumeration used in roll function
         typedef enum faciem { PASS, LEFT, RIGHT, CENTER } faces;
         faces die[] = { LEFT, RIGHT, CENTER, PASS, PASS, PASS };
-        
-        if(dice_roll != 0){// this print statement does not print for people who do not roll.
-	for (uint32_t i = 0; i < 1; i++){
-	printf("%s rolls...", philosophers[pos]);
-        }
-        for (uint32_t i = 0; i < dice_roll; i++) {
-	    faces face_die = die[random() % 6];
-            switch (face_die) {
-            case LEFT:
-                printf(" gives $1 to %s", philosophers[left_person(pos, players)]);
-                money[pos] = money[pos] - 1;
-                money[left_person(pos, players)] += 1;
-                break;
-            case RIGHT:
-                printf(" gives $1 to %s", philosophers[right_person(pos, players)]);
-                money[pos] = money[pos] - 1;
-                money[right_person(pos, players)] += 1;
-                break;
-            case CENTER:
-                printf(" puts $1 in the pot");
-                money[pos] = money[pos] - 1;
-                money_pot = money_pot + 1;
-                break;
-            default: printf(" gets a pass"); break;
+
+        if (dice_roll != 0) { // this print statement does not print for people who do not roll.
+            for (uint32_t i = 0; i < 1; i++) {
+                printf("%s rolls...", philosophers[pos]);
+            }
+            for (uint32_t i = 0; i < dice_roll; i++) {
+                faces face_die = die[random() % 6];
+                switch (face_die) {
+                case LEFT:
+                    printf(" gives $1 to %s", philosophers[left_person(pos, players)]);
+                    money[pos] = money[pos] - 1;
+                    money[left_person(pos, players)] += 1;
+                    break;
+                case RIGHT:
+                    printf(" gives $1 to %s", philosophers[right_person(pos, players)]);
+                    money[pos] = money[pos] - 1;
+                    money[right_person(pos, players)] += 1;
+                    break;
+                case CENTER:
+                    printf(" puts $1 in the pot");
+                    money[pos] = money[pos] - 1;
+                    money_pot = money_pot + 1;
+                    break;
+                default: printf(" gets a pass"); break;
+                }
             }
         }
-	}
         //checking for winner
         players_in = 0;
         for (uint32_t j = 0; j < players;
@@ -109,24 +109,23 @@ int main(void) {
         }
 
         if (players_in == 1) {
-	uint32_t winner_money = 0;
-        for(uint32_t i = 0; i < players; i++){
-	    if(money[i] != 0){
-            winner_money = money[i];	    
-            printf("\n%s wins the $%d pot with $%d left in the bank!\n", philosophers[i],
-                money_pot, money[i]);
-            return 0;}
-	}
+            uint32_t winner_money = 0;
+            for (uint32_t i = 0; i < players; i++) {
+                if (money[i] != 0) {
+                    winner_money = money[i];
+                    printf("\n%s wins the $%d pot with $%d left in the bank!\n", philosophers[i],
+                        money_pot, money[i]);
+                    return 0;
+                }
+            }
+        }
 
-	    }
-	
         //next iteration
-	if(dice_roll > 0){// so no unnecessary new lines would print
-	printf("\n");
-	}
-	pos = (pos + 1) % players;
+        if (dice_roll > 0) { // so no unnecessary new lines would print
+            printf("\n");
+        }
+        pos = (pos + 1) % players;
         continue;
     }
     return 0;
 }
-
--- asgn1/philos.h
+++ asgn1/philos.h
@@ -1,5 +1,6 @@
-#ifndef philos.h
-#define philos.h
+#ifndef PHILOS_H_
+#define PHILOS_H_
+
 char *philosophers[] = { "Immanuel Kant", "Martin Heidegger", "David Hume",
     "Georg Wilhelm Friedrich Hegel", "Arthur Schopenhauer", "Ludwig Wittgenstein",
     "Karl Wilhelm Friedrich Schlegel", "Friedrich Nietzsche", "Socrates", "John Stuart Mill",
