82eddc2708f6ae2f9f2084ea8aa59322d4ccdd05
siaulee <siaulee@ucsc.edu>
2021-05-13 14:18:04 -0700

    design doc and c files

---
+++ asgn6/decode.c
@@ -0,0 +1,36 @@
+#include <inttypes.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#define OPTIONS "hi:o:v"
+
+int main(int argc, char **argv) {
+    int opt = 0;
+    FILE *in = stdin, *out = stdout;
+    int verbose = 0;
+
+    while ((opt = getopt(argc, argv, OPTIONS)) != -1) {
+        switch (opt) {
+        case 'h':
+            return 0;
+        case 'i':
+            if ((in = fopen(optarg, "r")) == NULL) {
+                fprintf(stderr, "failed to open infile\n");
+                exit(1);
+            }
+            break;
+        case 'o':
+            if ((out = fopen(optarg, "w")) == NULL) {
+                fprintf(stderr, "failed to write outfile\n");
+                exit(1);
+            }
+            break;
+        case 'v': verbose = 1; break;
+        }
+    }
+
